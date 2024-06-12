3f8d60a06add418e364f98e8883693630c5a3b28
luccasirineu <luccas19100@outlook.com>
2024-06-12 15:04:00 -0300

    criação do projeto

---
+++ src/test/java/com/br/luccasdev/api_bootcamp/ApiBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.luccasdev.api_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
