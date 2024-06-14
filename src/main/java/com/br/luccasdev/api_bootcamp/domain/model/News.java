7dd682854b31400b680be748e8dba93efc40822a
luccasirineu <luccas19100@outlook.com>
2024-06-14 16:34:02 -0300

    criação do model e repository

---
+++ src/main/java/com/br/luccasdev/api_bootcamp/domain/model/News.java
@@ -0,0 +1,9 @@
+package com.br.luccasdev.api_bootcamp.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News  extends  BaseItem{
+
+
+}
---
+++ src/main/java/com/br/luccasdev/api_bootcamp/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.br.luccasdev.api_bootcamp.domain.repository;
+
+import com.br.luccasdev.api_bootcamp.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
